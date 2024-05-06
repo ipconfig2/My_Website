ca9c559cc19f3f60fef7cbe525cea8ff890da886
Holden Weber <78152356+ipconfig2@users.noreply.github.com>
2024-05-06 09:59:52 -0400

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,23 @@
-/* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  var hamburger = document.querySelector('.hamburger');
+  var sidenav = document.getElementById('mySidenav');
+  hamburger.classList.toggle('active');
+  sidenav.style.width = sidenav.style.width === '250px' ? '0' : '250px';
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  var hamburger = document.querySelector('.hamburger');
+  var sidenav = document.getElementById('mySidenav');
+  hamburger.classList.remove('active');
+  sidenav.style.width = '0';
 }
+
+document.getElementById("downloadButton").addEventListener("click", function() {
+  var downloadLink = document.createElement("a");
+  downloadLink.href = "resume.pdf"; // Path
+  downloadLink.download = "Holden_Weber_Resume.pdf"; // Set the name for the downloaded file
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+});
+
